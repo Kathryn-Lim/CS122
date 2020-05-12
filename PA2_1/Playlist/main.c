780dcf44f9c9b0ecc21f9ffb31e004c9eb2755d2
Kathryn-Lim <63330154+Kathryn-Lim@users.noreply.github.com>
2020-05-12 04:46:18 -0700

    Add files via upload

---
+++ PA2_1/Playlist/main.c
@@ -0,0 +1,17 @@
+//=========================================================================================
+//Name: Kathryn Lim
+//CptS 122, Spring 2020 Lab Section 15L
+//PA2: Movie Playlist
+//Date: Feb 9, 2020
+//NOTE: Builds, but load function doesn't work, mainly due to allocation and modification of the head pointer.
+//=========================================================================================
+
+
+#include "Playlist.h"
+
+int main() {
+
+	menu();
+
+	return 0;
+}
